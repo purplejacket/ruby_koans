d45c807fa42a896ef83074d3837df636a3e661a4
Kai Middleton <kai@radar.(none)>
2010-07-18 14:55:15 -0700

    Ruby 1.9.x needs the test-unit gem; also, 1.9.2+ needs different usage for the 'require' command

--- koans/code_mash.rb
+++ koans/code_mash.rb
@@ -1,1 +1,1 @@
-require 'edgecase'
+require File.expand_path(File.join(File.dirname(__FILE__), 'edgecase'))
--- src/path_to_enlightenment.rb
+++ src/path_to_enlightenment.rb
@@ -1,29 +1,29 @@
 # The path to Ruby Enlightenment starts with the following:
 
-require 'about_asserts'
-require 'about_nil'
-require 'about_arrays'
-require 'about_array_assignment'
-require 'about_hashes'
-require 'about_strings'
-require 'about_methods'
-require 'about_constants'
-require 'about_control_statements'
-require 'about_true_and_false'
-require 'about_triangle_project'
-require 'about_exceptions'
-require 'about_triangle_project_2'
-require 'about_iteration'
-require 'about_blocks'
-require 'about_sandwich_code'
-require 'about_scoring_project'
-require 'about_classes'
-require 'about_open_classes'
-require 'about_dice_project'
-require 'about_inheritance'
-require 'about_modules'
-require 'about_scope'
-require 'about_class_methods'
-require 'about_message_passing'
-require 'about_proxy_object_project'
-require 'about_extra_credit'
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_asserts'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_nil'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_arrays'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_array_assignment'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_hashes'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_strings'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_methods'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_constants'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_control_statements'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_true_and_false'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_triangle_project'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_exceptions'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_triangle_project_2'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_iteration'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_blocks'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_sandwich_code'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_scoring_project'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_classes'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_open_classes'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_dice_project'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_inheritance'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_modules'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_scope'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_class_methods'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_message_passing'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_proxy_object_project'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'about_extra_credit'))
